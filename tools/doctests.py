7ae60e887ab7046929ccc91432b4e41e30fb7030
isaiah-debug <binghamisaiah177@gmail.com>
2026-04-24 22:19:05 -0700

    this is the start of the new files

---
+++ tools/doctests.py
@@ -0,0 +1,14 @@
+import doctest 
+
+# Tests examples inside a seperate text file
+doctest.testfile("README.md")
+import doctest 
+
+def add(a, b):
+    """
+    >>> add (2, 3)
+    5
+    """
+    return a + b
+
+    if __name__== "_"
